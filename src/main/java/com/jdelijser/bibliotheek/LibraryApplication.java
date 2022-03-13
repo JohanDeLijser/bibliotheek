a2a097835e0aec13a4d0c12d322a5c14b8c1f144
Johan de Lijser <johan@lyfter.nl>
2022-03-13 19:40:10 +0100

    Finish base for overview

--- src/main/java/com/jdelijser/bibliotheek/LibraryApplication.java
+++ src/main/java/com/jdelijser/bibliotheek/LibraryApplication.java
@@ -7,12 +7,12 @@ import javafx.stage.Stage;
 
 import java.io.IOException;
 
-public class HelloApplication extends Application {
+public class LibraryApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
-        FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("main-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        FXMLLoader fxmlLoader = new FXMLLoader(LibraryApplication.class.getResource("library-view.fxml"));
+        Scene scene = new Scene(fxmlLoader.load(), 800, 400);
+        stage.setTitle("Library");
         stage.setScene(scene);
         stage.show();
     }
---
+++ src/main/java/com/jdelijser/bibliotheek/LibraryController.java
@@ -0,0 +1,31 @@
+package com.jdelijser.bibliotheek;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class LibraryController {
+    @FXML
+    protected void openGenres() {
+
+    }
+
+    @FXML
+    protected void openAuthors() {
+
+    }
+
+    @FXML
+    protected void openPublishers() {
+
+    }
+
+    @FXML
+    protected void addBook() {
+
+    }
+
+    @FXML
+    protected void deleteBook() {
+
+    }
+}
