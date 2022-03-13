3021f002ca83464c1fdfe249d4fcd01b6d2cf791
Johan de Lijser <johan@lyfter.nl>
2022-03-13 19:04:39 +0100

    Base for application

---
+++ src/main/java/com/jdelijser/bibliotheek/HelloController.java
@@ -0,0 +1,14 @@
+package com.jdelijser.bibliotheek;
+
+import javafx.fxml.FXML;
+import javafx.scene.control.Label;
+
+public class HelloController {
+    @FXML
+    private Label welcomeText;
+
+    @FXML
+    protected void onHelloButtonClick() {
+        welcomeText.setText("Welcome to JavaFX Application!");
+    }
+}
