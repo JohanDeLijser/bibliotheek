134e11a3c350e8bf28391e0f0a90c81a14ed0e7d
Johan de Lijser <johan@lyfter.nl>
2022-06-27 17:50:07 +0200

    Add last changes for storage

--- src/main/java/com/jdelijser/bibliotheek/LibraryApplication.java
+++ src/main/java/com/jdelijser/bibliotheek/LibraryApplication.java
@@ -1,14 +1,10 @@
 package com.jdelijser.bibliotheek;
 
 import com.jdelijser.bibliotheek.controller.SceneController;
-import com.jdelijser.bibliotheek.services.DatabaseService;
 import javafx.application.Application;
-import javafx.fxml.FXMLLoader;
-import javafx.scene.Scene;
 import javafx.stage.Stage;
 
 import java.io.IOException;
-import java.sql.Connection;
 import java.sql.SQLException;
 
 public class LibraryApplication extends Application {
